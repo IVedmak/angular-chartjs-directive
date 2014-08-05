d2cb0c4625e680aca6c1ee13828ed8c724c392e4
IVedmak <igor.iris@gmail.com>
2014-08-05 12:20:43 +0300

    Update chartjs-directive.js
    added additional listener for more granular data changes

--- chartjs-directive.js
+++ chartjs-directive.js
@@ -1,43 +1,55 @@
 'use strict';
 
 angular.module('chartjs-directive', []).
-  directive('chart', function () {
-    var baseWidth = 600;
-    var baseHeight = 400;
+    directive('chart', function () {
+        var baseWidth = 120;
+        var baseHeight = 100;
 
-    return {
-      restrict: 'E',
-      template: '<canvas></canvas>',
-      scope: {
-        chartObject: "=value"
-      },
-      link: function (scope, element, attrs) {
-        var canvas  = element.find('canvas')[0],
-            context = canvas.getContext('2d'),
-            chart;
+        return {
+            restrict: 'E',
+            template: '<canvas></canvas>',
+            scope: {
+                chartObject: "=value"
+            },
+            link: function (scope, element, attrs) {
+                var canvas = element.find('canvas')[0],
+                    context = canvas.getContext('2d'),
+                    chart;
 
-        var options = {
-          type:   attrs.type   || "Line",
-          width:  attrs.width  || baseWidth,
-          height: attrs.height || baseHeight
-        };
-        canvas.width = options.width;
-        canvas.height = options.height;
-        chart = new Chart(context);
+                var options = {
+                    type: attrs.type || "Line",
+                    width: attrs.width || baseWidth,
+                    height: attrs.height || baseHeight
+                };
+                canvas.width = options.width;
+                canvas.height = options.height;
+                chart = new Chart(context);
 
-        scope.$watch(function(){ return element.attr('type'); }, function(value){
-          if(!value) return;
-          options.type = value;
-          var chartType = options.type;
-          chart[chartType](scope.chartObject.data, scope.chartObject.options);
-        });
 
-        //Update when charts data changes
-        scope.$watch(function() { return scope.chartObject; }, function(value) {
-          if(!value) return;
-          var chartType = options.type;
-          chart[chartType](scope.chartObject.data, scope.chartObject.options);
-        });
-      }
-    }
-  });
+                scope.$watch(function () {
+                    return element.attr('type');
+                }, function (value) {
+                    if (!value) return;
+                    options.type = value;
+                    var chartType = options.type;
+                    chart[chartType](scope.chartObject.data, scope.chartObject.options);
+                });
+
+                var dataUpdateListener = function (value) {
+                    if (!value) return;
+                    var chartType = options.type;
+                    chart[chartType](scope.chartObject.data, scope.chartObject.options);
+                };
+
+                //Update when charts data changes
+                scope.$watch(function () {
+                    return scope.chartObject.data;
+                }, dataUpdateListener);
+
+                //Update when charts data changes without changing the whole object
+                scope.$watch(function () {
+                    return scope.chartObject;
+                }, dataUpdateListener);
+            }
+        }
+    });
